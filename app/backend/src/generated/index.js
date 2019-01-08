75850fc7844f2bed57429dede95f2be5012d7822
dashcraft <ashcraftbookkeeping@gmail.com>
2019-01-07 22:37:19 -0600

    Update master branch

--- app/backend/src/generated/index.js
+++ app/backend/src/generated/index.js
@@ -40,6 +40,40 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://us1.prisma.sh/daniel-ashcraft-a99d55/earth_guardians/dev`
+  endpoint: `https://eg-production-879cf73477.herokuapp.com/`
 });
 exports.prisma = new exports.Prisma();
+var models = [
+  {
+    name: "Action",
+    embedded: false
+  },
+  {
+    name: "ActionCategory",
+    embedded: false
+  },
+  {
+    name: "CommunityEvent",
+    embedded: false
+  },
+  {
+    name: "EventAction",
+    embedded: false
+  },
+  {
+    name: "Petition",
+    embedded: false
+  },
+  {
+    name: "Role",
+    embedded: false
+  },
+  {
+    name: "Schedule",
+    embedded: false
+  },
+  {
+    name: "User",
+    embedded: false
+  }
+];
